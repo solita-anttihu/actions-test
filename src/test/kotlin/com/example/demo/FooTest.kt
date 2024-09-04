c97e7e9b8e48988314ebd97c80559e3ab6734e52
Antti Huokko <antti.huokko@solita.fi>
2024-09-04 14:33:26 +0300

    Testing

--- src/test/kotlin/com/example/demo/FooTest.kt
+++ src/test/kotlin/com/example/demo/FooTest.kt
@@ -1,6 +1,5 @@
 package com.example.demo
 
-import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.fail
 import org.springframework.boot.test.context.SpringBootTest
@@ -8,7 +7,6 @@ import org.springframework.boot.test.context.SpringBootTest
 @SpringBootTest
 class FooTest {
 
-	@Disabled
 	@Test
 	fun testFail() {
 		fail("Testing failure")
