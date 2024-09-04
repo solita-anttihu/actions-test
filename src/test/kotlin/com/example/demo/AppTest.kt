ad06b68a5d7cb99f6ece5720bcaaff1cacf7c59d
Antti Huokko <antti.huokko@solita.fi>
2024-09-04 14:09:30 +0300

    Add faling test

--- src/test/kotlin/com/example/demo/AppTest.kt
+++ src/test/kotlin/com/example/demo/AppTest.kt
@@ -1,12 +1,20 @@
 package com.example.demo
 
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.fail
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
 class AppTest {
 
 	@Test
-	fun contextLoads() { }
+	fun testContextLoads() { }
+
+	// @Disabled
+	@Test
+	fun testFail() {
+		fail("Testing failure")
+	}
 
 }
